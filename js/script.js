90f97c7cffdb514dc95156889e10565bbd0a40c5
lee_c <gitleelee@gmail.com>
2023-11-18 07:11:57 -0800

    Finished event listener, for loops, and fixed comments.

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ const createVehicle = function (type, numWheels, color) {
     vehicle.type = type;
     vehicle.numWheels = numWheels;
     vehicle.color = color;
-  
+
     return vehicle;
-  };
-  // new objects using the createVehicle factory function
+};
+// new objects using the createVehicle factory function
 const car = createVehicle("car", "4", "black");
 const bike = createVehicle("bike", "2", "red");
 const skateboard = createVehicle("skateboard", "4", "brown");
@@ -21,14 +21,24 @@ const myVehicles = [car, bike, skateboard];
 
 // Loops over the array of objects, properties & values
 for (let vehicle of myVehicles) {
-  for (let key in vehicle) {
-    console.log(key, vehicle[key]);
-  }
+    for (let key in vehicle) {
+        console.log(key, vehicle[key]);
+    }
 }
-
-// Objects that need repair plus a event click button event
+// property called needsRepair
 bike.needsRepair = true;
 skateboard.needsRepair = true;
-
+// Click event, with function 
 button.addEventListener("click", function () {
-  repairList.innerHTML = "";
+    repairList.innerHTML = "";  // clear text
+    // New array that grabs the repair list
+    const vehicleRepairList = myVehicles.filter(function (vehicle) {
+        return vehicle.needsRepair === true;
+    });
+    // looping over objects to access array list
+    for (let vehicle of vehicleRepairList) {
+        let li = document.createElement("li");
+        li.innerHTML = `My <span>${vehicle.type}</span> needs some 💜`;
+        repairList.append(li);
+    }
+});
