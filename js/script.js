d95d5a79ff7e66686fb58004957a67b31a0383ce
lee_c <gitleelee@gmail.com>
2023-11-18 06:21:43 -0800

    Added new objects using createVehicle factory function.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,7 @@ const createVehicle = function (type, numWheels, color) {
   
     return vehicle;
   };
+  // new objects using the createVehicle factory function
+const car = createVehicle("car", "4", "black");
+const bike = createVehicle("bike", "2", "red");
+const skateboard = createVehicle("skateboard", "4", "brown");
